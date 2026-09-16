363c57b222bf2780b1d1b8706d05165d00129338
Vivek prasad <prasadvivek343@gmail.com>
2026-09-16 22:15:16 +0530

    app.py

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/lost")
+def lost():
+    return render_template("lost.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
